b979f1a93489f6604758630fb07d4a85b2bde66e
Michael Szwarc <msc@incowia.com>
2017-12-12 15:44:24 +0100

    Added code for IE11 compatibility

--- pathfinder/vtables/applicationlifecycleprojects/src/common/Utilities.js
+++ pathfinder/vtables/applicationlifecycleprojects/src/common/Utilities.js
@@ -1,3 +1,27 @@
+/* The MIT License (MIT)
+
+Copyright (c) 2017 LeanIX GmbH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+// from https://github.com/leanix/leanix-custom-reports
+
 function getCurrentLifecycle(node, timestamp) {
 	if (!hasLifecycle(node)) {
 		return;
@@ -5,7 +29,7 @@ function getCurrentLifecycle(node, timestamp) {
 	let result = undefined;
 	const now = timestamp ? timestamp : Date.now();
 	node.lifecycle.phases.forEach((e) => {
-		const lcDate = Date.parse(e.startDate + ' 00:00:00');
+		const lcDate = parseDateString(e.startDate);
 		if (lcDate <= now) {
 			result = e;
 		}
@@ -25,8 +49,17 @@ function createLifecycleObj(lifecyclePhase) {
 	return {
 		phase: lifecyclePhase.phase,
 		// that's a timestamp as number
-		startDate: Date.parse(lifecyclePhase.startDate + ' 00:00:00')
+		startDate: parseDateString(lifecyclePhase.startDate)
 	};
+}
+
+function parseDateString(date) {
+	const result = Date.parse(date + ' 00:00:00');
+	if (Number.isNaN(result)) {
+		const values = date.split('-');
+		return new Date(parseInt(values[0]), parseInt(values[1]) - 1, parseInt(values[2]));
+	}
+	return result;
 }
 
 function getLifecycleModel(setup, factsheetName) {
@@ -185,6 +218,7 @@ export default {
 	getLifecycleModel: getLifecycleModel,
 	getLifecycles: getLifecycles,
 	getLifecyclePhase: getLifecyclePhase,
+	parseDateString: parseDateString,
 	getFrom: getFrom,
 	createOptionsObj: createOptionsObj,
 	createOptionsObjFrom: createOptionsObjFrom,
